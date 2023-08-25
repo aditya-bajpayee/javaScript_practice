5cec4d165124548e4392546dabccca6b52c90f72
aditya-bajpayee <adityabajpayeesln@gmail.com>
2023-08-25 15:55:40 +0530

    add class

---
+++ Temp.java
@@ -0,0 +1,9 @@
+package javaScript_practice;
+
+import java.net.SocketPermission;
+
+public class Temp{
+    public static void main(String[] args) {
+        System.out.println("changes commited");
+    }
+}
